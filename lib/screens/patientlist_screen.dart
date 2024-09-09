4cb0851abefebc6fa25c28a7c10b430553b7af7c
Shubham-Pokhrel <contact@shubhampokhrel.com.np>
2024-09-09 20:03:56 +0545

    Logout button added for desktop view

--- lib/screens/patientlist_screen.dart
+++ lib/screens/patientlist_screen.dart
@@ -10,6 +10,33 @@ class PatientlistScreen extends StatefulWidget {
 class _PatientlistScreenState extends State<PatientlistScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xE4EBF1F5), // Background color similar to login screen
+      
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Your patient list content can go here
+            Text('Patient List Page Content Here'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context); // Navigate back to the previous screen (LoginScreen)
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF48A9F8), // Blue color similar to the Sign In button
+                foregroundColor: Colors.white, // White text color
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: Text('Logout'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
