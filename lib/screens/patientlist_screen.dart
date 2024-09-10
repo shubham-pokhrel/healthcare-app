63aed339a83ad2a2d10dc29756e65cbaa704e9b7
Shubham-Pokhrel <contact@shubhampokhrel.com.np>
2024-09-10 19:20:14 +0545

    search, sort, and basic framework designed

--- lib/screens/patientlist_screen.dart
+++ lib/screens/patientlist_screen.dart
@@ -24,92 +24,168 @@ class _PatientlistScreenState extends State<PatientlistScreen> {
         builder: (context, constraints) {
           maxW = constraints.maxWidth;
           maxH = constraints.maxHeight;
-          isMobile =
-              constraints.maxWidth <= 600; // Check if the screen is mobile
+          isMobile = constraints.maxWidth <= 600; // Check if the screen is mobile
 
           // Decide which patient data to display based on view
-          List<Patient> patients = isMobile
-              ? mobilePatients
-              : desktopPatients; // Fetch data based on screen type
+          List<Patient> patients = isMobile ? mobilePatients : desktopPatients; // Fetch data based on screen type
 
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 0),
             child: Column(
-              crossAxisAlignment: isMobile
-                  ? CrossAxisAlignment.start
-                  : CrossAxisAlignment.start,
+              crossAxisAlignment: isMobile ? CrossAxisAlignment.start : CrossAxisAlignment.start,
               children: [
                 if (!isMobile) ...[
                   // Desktop Layout
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(
-                            context); // Navigate back to the previous screen (LoginScreen)
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(
-                            0xFF48A9F8), // Blue color similar to the Sign In button
-                        foregroundColor: Colors.white, // White text color
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12.5),
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context); // Navigate back to the previous screen (LoginScreen)
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF48A9F8), // Blue color similar to the Sign In button
+                          foregroundColor: Colors.white, // White text color
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
                         ),
+                        child: Text('Log Out'),
                       ),
-                      child: Text('Logout'),
                     ),
                   ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Patient List",
+                  //SizedBox(height: 0),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 0.0, left: 50.0, right: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      //mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "Patient List",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            //height: 1.5,
+                            fontSize: constraints.maxWidth * 0.07 > 90 ? 90 : constraints.maxWidth*0.07,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black,
+                            fontFamily: 'Manjari',
+                          ),
+                        ),                        
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 50.0, right: 50.0, bottom: 15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ConstrainedBox(
+                          constraints: BoxConstraints(
+                            maxWidth: constraints.maxWidth * 0.31 > 460 ? 460 + constraints.maxWidth * 0.01 : constraints.maxWidth * 0.31,
+                            maxHeight: constraints.maxWidth * 0.2 > 50 ? 50 : constraints.maxWidth * 0.2,
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: ('Search by Patient, Bed No, Date etc.'),
+                              prefixIcon: Image.asset(
+                                'lib/assets/search_icon.png',
+                                height: 5,
+                              ),
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 10),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              filled: true,
+                              fillColor: Color(0xFFE4EBF1),
+                            ),
                             style: TextStyle(
-                              fontSize: 36,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
+                              fontSize: constraints.maxWidth * 0.02 > 24 ? 24 : constraints.maxWidth*0.02,
+                              fontWeight: FontWeight.w600,
                               fontFamily: 'Manjari',
                             ),
                           ),
-                          SizedBox(height: 8),
-                          Container(
-                            width: constraints.maxWidth * 0.8,
-                            padding: EdgeInsets.all(24),
-                            decoration: BoxDecoration(
-                              color: Color(
-                                  0xFFD3DDE5), // Background color similar to the login screen
-                              borderRadius: BorderRadius.circular(8),
+                        ),
+                        SizedBox(
+                            width: constraints.maxWidth * 0.1 > 8.0 ? 8.0 : constraints.maxWidth * 0.1),
+                        ConstrainedBox(
+                          constraints: BoxConstraints(
+                            maxWidth: constraints.maxWidth * 0.2 > 232 ? 232 + constraints.maxWidth * 0.01 : constraints.maxWidth * 0.2,
+                            maxHeight: constraints.maxWidth * 0.2 > 50 ? 50 : constraints.maxWidth * 0.2,
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: ('Sort By :'),
+                              suffixIcon: Image.asset(
+                                'lib/assets/drop_icon.png',
+                                height: 5,
+                              ),
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 10),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              filled: true,
+                              fillColor: Color(0xFFE4EBF1),
                             ),
-                            child: Center(
-                              child: Text('test'),
+                            style: TextStyle(
+                              fontSize: constraints.maxWidth * 0.02 > 24 ? 24 : constraints.maxWidth*0.02,
+                              fontWeight: FontWeight.w700,
+                              fontFamily: 'Manjari',
                             ),
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+                        Spacer(),
+                        ConstrainedBox(
+                          constraints: BoxConstraints(
+                            maxWidth: constraints.maxWidth * 0.15 > 173 ? 173 : constraints.maxWidth * 0.15,
+                            maxHeight: constraints.maxWidth * 0.2 > 50 ? 50 : constraints.maxWidth * 0.2,
+                          ),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // action here
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(
+                                  0xFF48A9F8), // Blue color similar to the Sign In button
+                              foregroundColor: Color(0xE4EBF1F5),
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 12, vertical: 10),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                            child: Text(
+                              '+ Add Patient',
+                              style: TextStyle(
+                                fontSize: constraints.maxWidth * 0.02 > 22 ? 22 : constraints.maxWidth*0.02,
+                                fontWeight: FontWeight.w700,
+                                fontFamily: 'Manjari',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                   // List of patients for Desktop
                   Padding(
                     padding: const EdgeInsets.only(
-                        right: 15.0, left: 15.0, bottom: 20.0),
+                        right: 50.0, left: 50.0, bottom: 20.0),
                     child: Container(
                       color: Color(0xFFD3D8DE),
                       padding: const EdgeInsets.only(
-                          right: 10.0, left: 10.0, bottom: 10.0, top: 10),
+                          right: 20.0, left: 20.0, bottom: 10.0, top: 20),
                       child: SizedBox(
-                        height: constraints.maxHeight * 0.65 > 557
-                            ? 557
-                            : constraints.maxHeight *
-                                0.65, // Adjust height to your needs
-                        width: constraints.maxWidth * 0.95 > 332
-                            ? 332 + constraints.maxWidth * 0.3
-                            : constraints.maxWidth * 0.95,
+                        height: constraints.maxHeight * 0.7 > 709 ? 709 : constraints.maxHeight * 0.7, // Adjust height to your needs
+                        width: constraints.maxWidth * 0.95 > 1272 ? 1272 + constraints.maxWidth * 0.3 : constraints.maxWidth * 0.95,
                         child: SingleChildScrollView(
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
@@ -120,11 +196,10 @@ class _PatientlistScreenState extends State<PatientlistScreen> {
                                     NeverScrollableScrollPhysics(), // Prevents inner scroll view conflict
                                 itemCount: patients.length,
                                 itemBuilder: (context, index) {
-                                  return buildPatientCardMobile(
+                                  return buildPatientCardDesktop(
                                       patients[index]);
                                 },
-                                separatorBuilder: (context, index) =>
-                                    SizedBox(height: 8),
+                                separatorBuilder: (context, index) =>  SizedBox(height: 2),
                               ),
                             ],
                           ),
@@ -311,13 +386,8 @@ class _PatientlistScreenState extends State<PatientlistScreen> {
                       padding: const EdgeInsets.only(
                           right: 10.0, left: 10.0, bottom: 10.0, top: 10),
                       child: SizedBox(
-                        height: constraints.maxHeight * 0.65 > 557
-                            ? 557
-                            : constraints.maxHeight *
-                                0.65, // Adjust height to your needs
-                        width: constraints.maxWidth * 0.95 > 332
-                            ? 332 + constraints.maxWidth * 0.3
-                            : constraints.maxWidth * 0.95,
+                        height: constraints.maxHeight * 0.65 > 557 ? 557 : constraints.maxHeight * 0.65, // Adjust height to your needs
+                        width: constraints.maxWidth * 0.95 > 332 ? 332 + constraints.maxWidth * 0.3 : constraints.maxWidth * 0.95,
                         child: SingleChildScrollView(
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
@@ -352,15 +422,17 @@ class _PatientlistScreenState extends State<PatientlistScreen> {
   // Method to build patient card for Desktop view
   Widget buildPatientCardDesktop(Patient patient) {
     String imagePath = patient.images.isNotEmpty
-        ? 'lib/assets/avatar_${patient.images}.png'
-        : 'lib/assets/avatar_1.png';
+        ? 'lib/assets/davatar_${patient.images}.png'
+        : 'lib/assets/davatar_1.png';
     //print('Patient image path: ${patient.images}');
+    //print(maxW);
+    //print(maxW* 0.03 > 40 ? 40 : maxW*0.03);
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 7.0),
       child: Container(
         // White background for the individual patient cards
-        height: maxH * 0.6 > 93 ? 93 : maxH * 0.6,
-        width: 300,
+        height: maxH * 0.8 > 199 ? 199 : maxH * 0.8,
+        width: 1120,
         decoration: BoxDecoration(
           color: Color(
               0xFFD3D8DE), // Background color for the patient details container
@@ -378,11 +450,11 @@ class _PatientlistScreenState extends State<PatientlistScreen> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               //print image from assests folder
-              Image.asset('lib/assets/avatar_${patient.images}.png',
-                  height: 40, width: 40),
+              Image.asset(imagePath,
+                  height: 150, width: 150),
               // Name and Info
               SizedBox(
-                width: 10,
+                width: 5,
               ),
               Row(
                 children: [
@@ -396,9 +468,11 @@ class _PatientlistScreenState extends State<PatientlistScreen> {
                             Text(
                               patient.name,
                               style: TextStyle(
-                                fontSize: 13,
+                                fontSize: maxW* 0.03 > 40 ? 40 : maxW*0.03,
+                                
                                 fontWeight: FontWeight.w700,
                                 fontFamily: 'Manjari',
+                                decoration: TextDecoration.underline,
                               ),
                             ),
                             SizedBox(width: 10),
